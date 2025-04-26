0ec8f17b8975b5dd029714d6e9f347b13cd60d85
brunorwanda4 <brunorwanda4@gmail.com>
2025-04-27 01:56:39 +0200

    make teacher card on class page

--- src/app/[lang]/(application)/c/[classId]/page.tsx
+++ src/app/[lang]/(application)/c/[classId]/page.tsx
@@ -1,3 +1,4 @@
+import ClassTeacherCard from "@/components/page/class/cards/class-teachers-card";
 import ClassHeader from "@/components/page/class/class-header";
 import { Separator } from "@/components/ui/separator";
 
@@ -6,8 +7,9 @@ const ClassIdPage = () => {
     <div className=" px-4 py-2 space-y-4">
       <ClassHeader />
       <Separator />
-      <div>
-        
+      <div className=" flex">
+        <ClassTeacherCard />
+        <div />
       </div>
     </div>
   );
---
+++ src/components/page/class/cards/class-teachers-card.tsx
@@ -0,0 +1,75 @@
+import MyImage from "@/components/myComponents/myImage";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+import { BsGear } from "react-icons/bs";
+
+const ClassTeacherCard = () => {
+  return (
+    <Card className=" w-1/2">
+      <CardHeader className=" flex justify-between">
+        <CardTitle>Teachers</CardTitle>
+        <Button variant={"primary"} library="daisy" size={"sm"}>
+          Add Teacher
+        </Button>
+      </CardHeader>
+      <CardContent className=" p-0">
+        <div className=" flex w-full justify-between hover:bg-base-300 card flex-row duration-200 p-2">
+          <div className=" flex space-x-2">
+            <MyImage
+              role="AVATAR"
+              src="/images/p.jpg"
+              className=" size-12"
+              classname=" mask mask-squircle"
+            />
+            <div>
+              <span className=" small-title">Teacher name</span>
+              <p>Kinyarwanda, english, Kiswahili</p>
+            </div>
+          </div>
+          <Button shape={"circle"} library="daisy" variant={"ghost"}>
+            <BsGear size={24}/>
+          </Button>
+        </div>
+        {/* end */}
+        <div className=" flex w-full justify-between hover:bg-base-300 card flex-row duration-200 p-2">
+          <div className=" flex space-x-2">
+            <MyImage
+              role="AVATAR"
+              src="/images/p.jpg"
+              className=" size-12"
+              classname=" mask mask-squircle"
+            />
+            <div>
+              <span className=" small-title">Teacher name</span>
+              <p>Kinyarwanda, english, Kiswahili</p>
+            </div>
+          </div>
+          <Button shape={"circle"} library="daisy" variant={"ghost"}>
+            <BsGear size={24}/>
+          </Button>
+        </div>
+        {/* end */}
+        <div className=" flex w-full justify-between hover:bg-base-300 card flex-row duration-200 p-2">
+          <div className=" flex space-x-2">
+            <MyImage
+              role="AVATAR"
+              src="/images/p.jpg"
+              className=" size-12"
+              classname=" mask mask-squircle"
+            />
+            <div>
+              <span className=" small-title">Teacher name</span>
+              <p>Kinyarwanda, english, Kiswahili</p>
+            </div>
+          </div>
+          <Button shape={"circle"} library="daisy" variant={"ghost"}>
+            <BsGear size={24}/>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ClassTeacherCard;
