937e9d8c22ec952686eaf39b4506ebc9cb723753
brunorwanda4 <brunorwanda4@gmail.com>
2025-11-28 01:43:38 +0200

    c

--- src/app/[lang]/(application)/c/[classUsername]/timetable/page.tsx
+++ src/app/[lang]/(application)/c/[classUsername]/timetable/page.tsx
@@ -1,8 +1,15 @@
+import TestCalendar from "@/components/test/test-calendar";
+
 const ClassTimetablePage = async (
   props: PageProps<"/[lang]/c/[classUsername]/timetable">,
 ) => {
   const params = await props.params;
-  return <div>class timetable page</div>;
+  return (
+    <div className=" flex  flex-col">
+      <h1 className=" h-3 mt-2">Class Time table</h1>
+      <TestCalendar />
+    </div>
+  );
 };
 
 export default ClassTimetablePage;
--- src/components/test/test-calendar.tsx
+++ src/components/test/test-calendar.tsx
@@ -150,7 +150,7 @@ export default function TestCalendar() {
   };
 
   return (
-    <div className=" px-4 py-4 space-y-4">
+    <div className=" space-y-4">
       <ClassTimetable
         events={events}
         // onEventAdd={handleEventAdd}
